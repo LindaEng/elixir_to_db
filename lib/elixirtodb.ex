bed8aa32a10b310feb71abae7e92a38488f104da
= <90lineng@gmail.com>
2023-03-01 18:28:22 -0500

    initial commit

---
+++ lib/elixirtodb.ex
@@ -0,0 +1,18 @@
+defmodule Elixirtodb do
+  @moduledoc """
+  Documentation for `Elixirtodb`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Elixirtodb.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixirtodb_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirtodbTest do
+  use ExUnit.Case
+  doctest Elixirtodb
+
+  test "greets the world" do
+    assert Elixirtodb.hello() == :world
+  end
+end
